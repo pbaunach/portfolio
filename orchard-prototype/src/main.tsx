d3eb96b5d3a52c7234b48487daf2a5c145ba38e5
Peebs <pbaunach@gmail.com>
2026-02-23 09:01:03 -0800

    remove password for orchard prototype for now

--- orchard-prototype/src/main.tsx
+++ orchard-prototype/src/main.tsx
@@ -2,12 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import PasswordGate from './components/PasswordGate.tsx'
+// import PasswordGate from './components/PasswordGate.tsx' // Re-enable for password protection
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    {/* Password protection temporarily disabled - uncomment to re-enable:
     <PasswordGate>
       <App />
     </PasswordGate>
+    */}
+    <App />
   </StrictMode>,
 )
